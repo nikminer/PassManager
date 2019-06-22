a144255376e7ac0000d264d942cb5262888bc04f
nikminer <nikminerfeed@gmail.com>
2019-06-22 16:34:44 +0300

    MasterPass window

--- PassManager/MainWindow.cs
+++ PassManager/MainWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Gtk;
+using System.IO;
 
 public partial class MainWindow : Gtk.Window
 {
--- PassManager/Program.cs
+++ PassManager/Program.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using Gtk;
-
+using System.IO;
 namespace PassManager
 {
+    class AppData
+    {
+        static public string Appdir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "/.passmanager";
+
+        static public string MastePassVariableName = "PassManager_MasterPass";
+
+        static public bool isChange = false;
+    }
     class MainClass
     {
         public static void Main(string[] args)
